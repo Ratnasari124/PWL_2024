d22aed5f124bb4ea1c4fa553e85b346131415553
Ratnasari <ratnasari121729@gmail.com>
2024-02-20 11:09:33 +0700

    Perubahan Route Di web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//PRAKTIKUM 1
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+//d
+Route::get('/world', function () {
+    return 'World';
+});
+//f
+Route::get('/selamat datang', function () {
+    return 'Selamat Datang';
+});
+//g
+Route::get('/about', function () {
+    return 'NIM : 2241720007 NAMA : RATNASARI';
+});
+
+//PRAKTIKUM 2
+
+//a
+Route::get('/user/{ratna}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+//d
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+//f
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel ' . $id;
+});
+
+//PRAKTIKUM 3
+//a
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+//d
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+//PRAKTIKUM 4
+//a
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
